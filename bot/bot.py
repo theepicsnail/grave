a6046ff946e2fd3fec280ab83138a05110aa7a89
Steven <theepicsnail@gmail.com>
2014-05-26 03:30:57 -0400

    Refactored to setUp/tearDown

--- bot/bot.py
+++ bot/bot.py
@@ -1,20 +1,29 @@
-import sys
 from irc import parse_message
 from queuereader import QueueReader
 
 class Bot(object):
+
     def __init__(self, input_queue, output_queue):
         self.pending_rpcs = []
         self.output_queue = output_queue
-        print "Bot started"
-        self.send("PRIVMSG #! :test2")
+        self.input_queue = input_queue
 
-        self.reader = QueueReader(input_queue, self.handle_line)
+    def setUp(self):
+        self.send("PRIVMSG #test :Started")
+        self.reader = QueueReader(self.input_queue, self.handle_line)
+
+    def tearDown(self):
+        self.reader.end()
+        self.send("PRIVMSG #test :Stopped")
 
     def send(self, msg):
         self.output_queue.put(msg)
 
     def handle_line(self, line):
+        if line is None:
+            self.tearDown()
+            return
+
         print "Bot handle line:", line
         prefix, command, args = parse_message(line)
 
