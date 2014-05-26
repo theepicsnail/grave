dbac5c472a0bcebdbc90f198559842970229ddeb
Steven <theepicsnail@gmail.com>
2014-05-26 02:34:51 -0400

    Adding connection_test

---
+++ bot/connection_test.py
@@ -0,0 +1,17 @@
+"""Tests for connection"""
+from bot.testing import SimpleTest
+from bot import connection
+class Connection(connection.Connection, SimpleTest):
+    def testEndToEnd(self):
+        self.simulate_msg("user", "#room", "!alarm now#msg")
+        self.assert_msg("#room", Alarm.SCHEDULED)
+        self.assert_msg("#room", "user: msg")
+
+    def _testUsage(self):
+        self.simulate_msg("user", "#room", "?alarm")
+        self.assert_msg("#room", Alarm.EXAMPLE)
+
+    def _testAutoExample(self):
+        self.simulate_msg("user", "#room", "!alarm adfadf")
+        self.assert_msg("#room", Alarm.EXAMPLE)
+
