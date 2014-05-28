e66e0b2719471816a142e08c66f419fc11e527e5
Steven <theepicsnail@gmail.com>
2014-05-28 17:01:34 -0400

    Fixed race condition. Added documentation

--- bot/queuereader.py
+++ bot/queuereader.py
@@ -1,23 +1,30 @@
+""" queuereader.py
+Read from a multiprocessing queue, and call a callback with the
+retreived data.
+"""
 import threading
 
 class QueueReader(object):
+    """ Async reader for multiprocessing.queues.
+    Given a callback, call the callback with each item pulled from the queue.
+    """
     def __init__(self, queue, callback):
         self.queue = queue
         self.callback = callback
-        self.running = False
+        self.running = True
         self.thread = threading.Thread(target=self.__read)
         self.thread.start()
 
     def __read(self):
-        self.running = True
+        """ Main loop, started on another thread.
+        Consume the queue, calling the callback on each item"""
         while self.running:
-            print "Waiting..."
             val = self.queue.get()
-            print "... got:" , val
             if self.running:
                 self.callback(val)
 
     def end(self):
+        """ Shut down the queue reader. """
         self.running = False
         self.queue.put(None)
 
