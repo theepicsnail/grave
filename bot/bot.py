16b1d57022e00ba6cb1ceca99ab97f1830f33a70
Steven <theepicsnail@gmail.com>
2014-06-01 05:24:43 -0400

    Cleaned up bot a bit

--- bot/bot.py
+++ bot/bot.py
@@ -9,23 +9,27 @@ class Bot(object):
         self.pending_rpcs = []
         self.output_queue = output_queue
         self.input_queue = input_queue
+        self.setUp()
 
     def setUp(self):
+        self.send("JOIN #test")
         self.send("PRIVMSG #test :Started")
         self.reader = QueueReader(self.input_queue, self.handle_line)
 
     def tearDown(self):
-        self.reader.end()
         self.send("PRIVMSG #test :Stopped")
+        self.reader.end()
 
     def send(self, msg):
         self.output_queue.put(msg)
 
     def handle_line(self, line):
+        print "Handle line:", line
         if line is None:
             self.tearDown()
             return
-
-        prefix, command, args = parse_message(line)
-
+        prefix, command, args = line
+        print "Bot:", prefix, command, args
+        import sys
+        sys.stdout.flush()
 
