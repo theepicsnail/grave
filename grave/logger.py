468e27e7d6428b25461385aef7e2896f43ae963a
Steven <theepicsnail@gmail.com>
2014-06-03 11:51:16 -0400

    Added coloring to ConsoleFormatter

--- grave/logger.py
+++ grave/logger.py
@@ -6,13 +6,70 @@ import logging.config
 import threading
 import os
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
-from logging import getLogger
+from logging import  currentframe
+import hashlib
+
+loaded = False
+def getLogger(*a):
+    if not loaded:
+        loadLogConfig()
+    return logging.getLogger(*a)
+
+class ConsoleFormatter(logging.Formatter):
+    RESET       = "\033[0m"
+    BLACK       = "\033[30m"
+    RED         = "\033[31m"
+    GREEN       = "\033[32m"
+    YELLOW      = "\033[33m"
+    BLUE        = "\033[34m"
+    PURPLE      = "\033[35m"
+    CYAN        = "\033[36m"
+    LIGHT_GRAY  = "\033[37m"
+    DARK_GRAY   = "\033[1;30m"
+    LIGHT_RED   = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    LIGHT_YELLOW= "\033[1;33m"
+    LIGHT_BLUE  = "\033[1;34m"
+    LIGHT_PURPLE= "\033[1;35m"
+    LIGHT_CYAN  = "\033[1;36m"
+    WHITE       = "\033[1;37m"
+
+    def hashColor(self, value):
+        return [
+            ConsoleFormatter.RED,
+            ConsoleFormatter.GREEN,
+            ConsoleFormatter.YELLOW,
+            ConsoleFormatter.BLUE,
+            ConsoleFormatter.PURPLE,
+            ConsoleFormatter.CYAN,
+            ConsoleFormatter.WHITE,
+        ][sum(map(ord, hashlib.sha1(str(value)).digest()))%7]
+
+    def format(self, record):
+        record.reset_color = ConsoleFormatter.RESET
+        #'created', 'exc_info', 'exc_text', 'filename', 'funcName', 'getMessage',
+        #'levelname', 'levelno', 'lineno', 'module', 'msecs', 'msg', 'name',
+        #'pathname', 'process', 'processName', 'relativeCreated', 'thread', 'threadName'
+        record.level_color = {
+            "DEBUG": ConsoleFormatter.LIGHT_GRAY,
+            "INFO": ConsoleFormatter.CYAN,
+            "WARNING": ConsoleFormatter.YELLOW,
+            "ERROR": ConsoleFormatter.RED,
+            "CRITICAL": "\033[1;41m"
+        }[record.levelname]
+
+        record.process_color = self.hashColor(record.process)
+        record.thread_color = self.hashColor(record.thread)
+
+        return super(ConsoleFormatter, self).format(record)
+
+    def formatTime(self, record, datefmt=None):
+        s = super(ConsoleFormatter, self).formatTime(record, datefmt)
+        return ConsoleFormatter.YELLOW + s + ConsoleFormatter.RESET
 
 def loadLogConfig():
     logging.config.fileConfig("logging/logging.cfg")
     print "Loaded logging config"
-loadLogConfig()
-
     global loaded
     loaded = True
     getLogger().debug("debug")
@@ -31,18 +88,6 @@ def logged(cls):
         if callable(getattr(cls, attr)):
             setattr(cls, attr, _log_calls(getattr(cls, attr)))
     return cls
-
-#def getLogger(name=""):
-#    calling_thread = threading.currentThread()
-#    thread_name = calling_thread.getName()
-#    thread_id = calling_thread.ident
-#    pid = os.getpid()
-#    log_name = "Process - %s.%s(%s)" % (
-#        pid, thread_name, thread_id)
-#    if name:
-#        log_name += "." + name
-#    return logging.getLogger()#log_name)
-
 
 import traceback
 def _log_calls(method):
